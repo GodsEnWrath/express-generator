6294aa2f9ed9eafb4463f93de1d34c55aa64ec05
GodsEnWrath <fastyazid@gmail.com>
2019-11-26 14:43:59 +0700

    add home

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/home", (req, res) => {
+  res.json({ message: "INI HOME"})
+})
 
 module.exports = router;
