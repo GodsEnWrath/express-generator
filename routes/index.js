a9b3a2956584b6506990e06c4034c5a59d581584
GodsEnWrath <fastyazid@gmail.com>
2019-11-26 16:32:16 +0700

    3 commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res) {
-  res.render("index", { title: "Express" });
-});
-
-router.get("/home", (req, res) => {
-  res.json({ message: "ini adalah route home" });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
